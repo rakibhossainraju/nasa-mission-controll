ed8654c302423237af2d4b92cda860cca0ae2842
rakibhossainraju <rakibhossianraju@gmail.com>
2024-12-31 17:11:13 +0600

    Did some refactor to the code

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,8 @@
 import * as http from "http";
 import { app } from "./app.js";
 import "dotenv/config";
-import * as mongoose from "mongoose";
+import mongoose from "mongoose";
 import { logMagenta } from "./utility/logger.js";
-// import { loadHabitablePlanet } from "./models/planets.model.js";
 
 const server = http.createServer(app);
 const PORT = process.env.PORT || 8000;
@@ -11,7 +10,6 @@ const MONGO_DB_CONNECTION_STRING = process.env.MONGO_DB_CONNECTION_STRING;
 
 try {
   await mongoose.connect(MONGO_DB_CONNECTION_STRING);
-  // await loadHabitablePlanet()
   server.listen(PORT, () => {
     console.log("Your server is listening to port", PORT, "...");
     logMagenta(`http://localhost:${PORT}/`);
@@ -19,3 +17,4 @@ try {
 } catch (error) {
   console.error("MongoDb connection failed ", error);
 }
+
