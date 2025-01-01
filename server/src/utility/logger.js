c88f9638a19d72910234e55eb09dcf8c2dc1f846
rakibhossainraju <rakibhossianraju@gmail.com>
2025-01-01 13:24:17 +0600

    refactor: simplify logging utility with color functions

--- server/src/utility/logger.js
+++ server/src/utility/logger.js
@@ -1,16 +1,19 @@
 import chalk from "chalk";
 
-export const logBlue = (message) => {
+const createLogger = (color) => (message) => {
   if (process.env.NODE_ENV === "development") {
-    console.log(chalk.blue(message));
+    console.log(chalk[color](message));
   } else {
     console.log(message);
   }
 };
-export const logMagenta = (message) => {
+const createLoggerWithBackground = (color) => (message) => {
   if (process.env.NODE_ENV === "development") {
-    console.log(chalk.magenta(message));
+    console.log(chalk[`bg${color}`].black.bold(message));
   } else {
-    // console.log(message);
+    console.log(message);
   }
 };
+export const logBlue = createLogger("blue");
+export const logYellow = createLogger("yellow");
+export const logMagenta = createLoggerWithBackground("Magenta");
