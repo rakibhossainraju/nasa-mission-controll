a0bd6ebf8b4b91b20656ab31d9030615d96ea197
rakibhossainraju <rakibhossianraju@gmail.com>
2025-01-11 17:56:41 +0600

    Refactored the code

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,16 @@
-import * as http from "http";
-import { app } from "./app.js";
 import "dotenv/config";
+import http from "http";
+import { app } from "./app.js";
 import { logMagenta } from "./utility/logger.js";
 import { connectMongoDb } from "./services/mongo.js";
+import { loadLaunchData } from "./models/launches.model.js";
 
 const server = http.createServer(app);
 const PORT = process.env.PORT || 8000;
 
 try {
   await connectMongoDb();
+  await loadLaunchData();
   server.listen(PORT, () => {
     logMagenta(`http://localhost:${PORT}/`);
   });
